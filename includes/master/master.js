22356e8cf674e0faf28711f632fdc071b5d8315a
Remco Visser <rvremco@hotmail.com>
2016-11-02 13:53:16 +0100

    Beginning of the admin pannel

--- includes/master/master.js
+++ includes/master/master.js
@@ -1,10 +1,10 @@
-document.writeln('<script type="text/javascript" src="../includes/js/jquery-3.1.1.js"></script>');
+var root = location.protocol + '//' + location.host;
+
+document.writeln('<script type="text/javascript" src="'+root+'/includes/js/jquery-3.1.1.js"></script>');
 document.writeln('<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.18/vue.min.js"></script>');
 
 document.onreadystatechange = function () {
   if (document.readyState == 'complete' ) {
-
-    var root = location.protocol + '//' + location.host;
 
     $("head" ).load( root + "/includes/master/partials/head.html" );
 
