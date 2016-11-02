dd2dee25ee6638521865a5ab1913b5eb6b43c176
Remco Visser <rvremco@hotmail.com>
2016-11-02 20:13:22 +0100

    Added products plot

--- admin/plots/index.js
+++ admin/plots/index.js
@@ -2,17 +2,17 @@ $(document).ready(function() {
   document.title += 'plots';
 });
 
-var userData;
-var xhrExist = new XMLHttpRequest();
-xhrExist.open('GET', local + '/admin/registered-users');
-xhrExist.onload = function () {
-  createUserChart(JSON.parse(xhrExist.responseText));
+// Users chart
+var xhrUsers = new XMLHttpRequest();
+xhrUsers.open('GET', local + '/admin/registered-users');
+xhrUsers.onload = function () {
+  createUserChart(JSON.parse(xhrUsers.responseText));
 }
-xhrExist.send();
+xhrUsers.send();
 
 function createUserChart(data){
-  var ctx = document.getElementById('myChart').getContext('2d');
-    var myChart = new Chart(ctx, {
+    var ctx = document.getElementById('usersChart').getContext('2d');
+    var userchart = new Chart(ctx, {
     type: 'line',
     data: {
       labels: data[0].users,
@@ -24,3 +24,35 @@ function createUserChart(data){
     }
   });
 }
+
+// Products chart
+var xhrProducts = new XMLHttpRequest();
+xhrProducts.open('GET', local + '/admin/best-selling-products/10');
+xhrProducts.onload = function () {
+  createProductsChart(JSON.parse(xhrProducts.responseText));
+}
+xhrProducts.send();
+
+function createProductsChart(data){
+  var hoverData = data[0].productnames;
+  var amountData = data[0].amount;
+  var backgroundColors = ["#FF6384", "#36A2EB","#FFCE56", "#4BC0C0", "#7743CE", "#8e44ad", "#27ae60", "#e67e22", "#d35400", "#2c3e50"];
+
+  var data = {
+      labels: hoverData,
+      datasets: [
+          {
+              data: amountData,
+              backgroundColor: backgroundColors,
+              hoverBackgroundColor: backgroundColors
+          }]
+  };
+  var ctx = document.getElementById('productsChart').getContext('2d');
+  var productsChart = new Chart(ctx,{
+      type: 'pie',
+      data: data
+  });
+}
+
+
+// Orders chart
