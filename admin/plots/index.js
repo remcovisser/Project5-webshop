21bc848eec431e7fe88c938e32e3f3e45532b2c9
Remco Visser <rvremco@hotmail.com>
2016-11-02 15:49:32 +0100

    Added users chart

--- admin/plots/index.js
+++ admin/plots/index.js
@@ -1,3 +1,26 @@
 $(document).ready(function() {
-      document.title += 'plots';
+  document.title += 'plots';
+});
+
+var userData;
+var xhrExist = new XMLHttpRequest();
+xhrExist.open('GET', local + '/admin/registered-users');
+xhrExist.onload = function () {
+  createUserChart(JSON.parse(xhrExist.responseText));
+}
+xhrExist.send();
+
+function createUserChart(data){
+  var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: data[0].users,
+      datasets: [{
+        label: 'Total registered users per day',
+        data: data[0].day,
+        backgroundColor: "rgba(153,255,51,0.4)"
+      }]
+    }
   });
+}
--- includes/master/master.js
+++ includes/master/master.js
@@ -1,4 +1,6 @@
 var root = location.protocol + '//' + location.host;
+var local = 'http://localhost:8080/';
+var live  = 'http://95.85.32.165:8080/';
 
 document.writeln('<script type="text/javascript" src="'+root+'/includes/js/jquery-3.1.1.js"></script>');
 document.writeln('<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.18/vue.min.js"></script>');
