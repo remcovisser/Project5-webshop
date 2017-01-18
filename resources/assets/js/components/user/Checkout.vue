d341e1d5afe6c9bc519fb4292dd7458e4d7278a0
Remco Visser <rvremco@hotmail.com>
2017-01-18 11:38:02 +0100

    Finished order logic

--- resources/assets/js/components/user/Checkout.vue
+++ resources/assets/js/components/user/Checkout.vue
@@ -72,58 +72,81 @@ export default {
   methods: {
     finishPayment: function() {
       // Get the required address data
+      var firstname =  $("#firstname").val();
+      var lastname = $("#lastname").val();
+      var phonenumber = $("#phonenumber").val();
       var street = $("#street").val();
       var city = $("#city").val();
       var country = $("#country").val();
       var postalcode = $("#postalcode").val();
       var housenumber = $("#housenumber").val();
+      var user_id = $.cookie('user').user_id;
+      var nextStep = true;
 
-      // TODO: validation of the fields
-
-      createAddress(street, city, country, postalcode, housenumber, insertOrder);
-
-      function insertOrder(address_id) {
-        var values = {
-          user_id: 1,
-          firstname: $("#firstname").val(),
-          lastname: $("#lastname").val(),
-          phonenumber: $("#phonenumber").val(),
-          address_id: address_id
-        };
-
-        $.post(local + '/orders/create', JSON.stringify(values), function(result) {
-          insertOrderlines();
-        });
+      // Validation of the fields
+      if(!$("#agree").is(':checked')) {
+        nextStep = false;
+        message("danger", "You have the agree with the Terms and Conditions.");
+      }
+      if (firstname == "" || lastname == "" || phonenumber == "" || street == "" || city == "" || postalcode == "" || housenumber == "") {
+        nextStep = false;
+        message("danger", "Please fill in all the required fields.");
       }
 
-      function insertOrderlines() {
-        $.get(local + '/orders/last', function(lastOrderId) {
-          var order_id;
-          if(lastOrderId == 'undefined') {
-              order_id = 1;
-          } else {
-              order_id = lastOrderId + 1;
-          }
-          var cartCookie = $.cookie('cart');
-          for (var i = 0; i < cartCookie.length; i++) {
-            $.ajax({
-                url: local + '/products/' + cartCookie[i]["p"],
-                async: false,
-                dataType: 'json',
-                success: function(product) {
-                  var values = {
-                    product_id: cartCookie[i]["p"],
-                    order_id: order_id,
-                    quantity: cartCookie[i]["q"],
-                    product_price: product[0]['p_price']
-                  };
-                  $.post(local + '/orderlines/create', JSON.stringify(values), function(result) {});
-                }
-            });
-            if(i+1 == cartCookie.length)
-              message("success", "Your order has been paid");
-          }
-        });
+      // If there are no errors
+      if(nextStep) {
+        // Select or create the address based on the input data from the user
+        createAddress(street, city, country, postalcode, housenumber, insertOrder);
+
+        function insertOrder(address_id) {
+          var values = {
+            user_id: user_id,
+            firstname: firstname,
+            lastname: lastname,
+            phonenumber: phonenumber,
+            address_id: address_id
+          };
+
+          $.post(local + '/orders/create', JSON.stringify(values), function(result) {
+            insertOrderlines();
+          });
+        }
+
+        function insertOrderlines() {
+          $.get(local + '/orders/last', function(lastOrderId) {
+            console.log(lastOrderId);
+            var order_id = lastOrderId[0]['order_id'] + 1;
+
+            // Get the product info to store it in the orderline
+            var cartCookie = $.cookie('cart');
+            for (var i = 0; i < cartCookie.length; i++) {
+              $.ajax({
+                  url: local + '/products/' + cartCookie[i]["p"],
+                  async: false,
+                  dataType: 'json',
+                  success: function(product) {
+                    var values = {
+                      product_id: cartCookie[i]["p"],
+                      order_id: order_id,
+                      quantity: cartCookie[i]["q"],
+                      product_price: product[0]['p_price']
+                    };
+                    $.post(local + '/orderlines/create', JSON.stringify(values), function(result) {});
+                  }
+              });
+              // Last itteration
+              if(i+1 == cartCookie.length)
+                // TODO empty cart, ridirect
+                message("success", "Your order has been paid, you will be ridirected to the index in 5 seconds");
+                setInterval(orderFinished, 5000)
+            }
+          });
+        }
+        
+        function orderFinished() {
+            $.removeCookie('cart', { path: '/' });
+            window.location.replace("/");
+        }
       }
     }
   }
