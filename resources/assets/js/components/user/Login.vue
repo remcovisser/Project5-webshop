9d3661511ba6968890e6d86d3c8096a9f2ee5fa0
Bunyu93 <b-40@live.nl>
2017-01-20 18:24:29 +0100

    only need to fix cookie not getting lost on refresh

--- resources/assets/js/components/user/Login.vue
+++ resources/assets/js/components/user/Login.vue
@@ -4,11 +4,11 @@
         <form>
             <div class="col-md-6 login-do">
                 <div class="login-mail">
-                    <input type="text" placeholder="Username" id="username">
+                    <input type="text" name="username" placeholder="Username" id="username">
                     <i class="glyphicon glyphicon-user"></i>
                 </div>
                 <div class="login-mail">
-                    <input type="password" placeholder="Password" id="password">
+                    <input type="password" name="password" placeholder="Password" id="password">
                     <i class="glyphicon glyphicon-lock"></i>
                 </div>
                 <a class="news-letter " href="#">
@@ -38,31 +38,78 @@ export default {
             if (username == "" || password == "") {
                 message("danger", "Please fill in all the fields.");
             } else {
-                $.get(local + 'users/' + username + '/' + password, function(user) {
-                    if (user[0]) {
-                        var user_data = {
-                            "user_id": user[0].user_id,
-                            "username": user[0].username,
-                            "email": user[0].email,
-                            "date": user[0].date_of_birth,
-                            "admin": user[0].admin
-                        };
-                        if($('#rememberMe').is(":checked")) {
-                          expireTime = 7;
-                        } else {
-                          expireTime = 1;
-                        }
-                        $.cookie('user', user_data, {
-                            expires: expireTime,
-                            path: '/'
-                        });
-                        window.location = '/';
-                    } else {
-                        message("danger", "Wrong username / password.");
-                    }
-                });
+                $.ajax({
+                    url: local + 'auth',
+                    type: 'POST',
+                    data: {
+                    'username': username,
+                    'password': password
+                    },
+                    success:function(result)//we got the response
+                    {                          
+                        if (result.success == 'true') {
+                            if($('#rememberMe').is(":checked")) {expireTime = 7;}
+                            else {expireTime = 1;}
+                            
+                            var user_data = {
+                                            "user_id": result.user_id,
+                                            "username": result.username,
+                                            "email": result.email,
+                                            "date": result.date_of_birth,
+                                            "admin": result.admin
+                                            };
+
+                            $.cookie('user',user_data, result.token, {
+                                token : result.token,
+                                expires: expireTime,
+                                path: '/'
+                            });
+                            window.location = '/';
+                        } 
+                        else 
+                        {
+                            message("danger", "Wrong username / password.");
+                        }   
+                    }})
             }
         }
     }
 }
+
+// export default {
+//     methods: {
+//         login: function() {
+//             var username = $("#username").val();
+//             var password = md5($("#password").val());
+//             var expireTime;
+//             if (username == "" || password == "") {
+//                 message("danger", "Please fill in all the fields.");
+//             } else {
+//                 $.get(local + 'users/' + username + '/' + password, function(user) {
+//                     if (user[0]) {
+//                         var user_data = {
+//                             "user_id": user[0].user_id,
+//                             "username": user[0].username,
+//                             "email": user[0].email,
+//                             "date": user[0].date_of_birth,
+//                             "admin": user[0].admin
+//                         };
+//                         if($('#rememberMe').is(":checked")) {
+//                           expireTime = 7;
+//                         } else {
+//                           expireTime = 1;
+//                         }
+//                         $.cookie('user', user_data, {
+//                             expires: expireTime,
+//                             path: '/'
+//                         });
+//                         window.location = '/';
+//                     } else {
+//                         message("danger", "Wrong username / password.");
+//                     }
+//                 });
+//             }
+//         }
+//     }
+// }
 </script>
